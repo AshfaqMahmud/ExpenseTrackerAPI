30ce24454d3d5eda009a40cf282992efbdaf3beb
Ashfaq Mahmud <ash.mse21@gmail.com>
2025-05-31 08:47:26 +0600

    modification

--- ExpenseTracker/Models/ExpenseDatabaseSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace ExpenseTracker.Models
-{
-    public class ExpenseDatabaseSettings
-    {
-        public string ConnectionString { get; set; } = string.Empty;
-        public string DatabaseName { get; set; } = string.Empty;
-        public string ExpensesCollectionName { get; set; } = string.Empty;
-    }
-}
--- ExpenseTracker/Program.cs
+++ ExpenseTracker/Program.cs
@@ -1,4 +1,3 @@
-
 using ExpenseTracker.Data;
 using ExpenseTracker.Models;
 using ExpenseTracker.Services;
@@ -23,20 +22,13 @@ internal class Program
         builder.Services.AddSingleton<BooksService>();
         // mongoDB configuration
 
-        builder.Services.Configure<ExpenseDatabaseSettings>(
-            builder.Configuration.GetSection("ExpenseDatabase")
-        );
-        builder.Services.AddSingleton<ExpenseService>();
+        
 
         builder.Services.AddControllers()
         .AddJsonOptions(
             options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
 
-        // builder.Services.AddDbContext<ApplicationDBContext>(options =>
-        //     options.UseSqlServer(
-        //         builder.Configuration.GetConnectionString("DefaultConnection")
-        //         )
-        // );
+      
         var app = builder.Build();
 
         if (app.Environment.IsDevelopment())
@@ -46,7 +38,6 @@ internal class Program
             app.UseSwaggerUI(
                 options =>
                 {
-            
                     options.SwaggerEndpoint("/swagger/v1/swagger.json", "Expense Tracker API V1");
                     
                 }       
